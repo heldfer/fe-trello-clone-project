c16029c6e30cbf2c20f566e7317fa7afa4f35a44
Anlh <heldern19@gmail.com>
2020-03-06 00:06:32 +0000

    installed all the feathers-vuex client dependencies and installed vuex store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import vuex from './plugins/vuex';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  vuex,
   render: h => h(App)
 }).$mount('#app')
