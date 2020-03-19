4ddde00b2b8ce9e5428b58bcd51ea6b6766d086a
Hélder Ferreira <helderemanuelferreira@gmail.com>
2020-03-19 22:17:16 +0000

    Created board form to create new boards

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -3,6 +3,7 @@
     class="fill-height"
     fluid
   >
+    {{ boards }}
     <v-row
       align="center"
       justify="center"
@@ -21,25 +22,47 @@
             Create a new Board
           </v-card-title>
         </v-row>
-        <v-card-actions>
-          <v-row
-            align="center"
-            justify="center"
-          >
-            <v-btn
-              color="primary"
-              @click="show = !show"
-            >
-              Create
-            </v-btn>
-          </v-row>
-        </v-card-actions>
         <v-expand-transition>
-          <div v-show="show">
+          <div class="text-center">
             <v-divider />
 
             <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              <v-form
+                v-if="!creating"
+                v-model="valid"
+                @keydown.prevent.enter
+                @submit.prevent="createBoard"
+              >
+                <v-text-field
+                  v-model="board.name"
+                  :rules="rules.required"
+                  label="Name"
+                  type="text"
+                />
+
+                <v-text-field
+                  v-model="board.background"
+                  :rules="rules.required"
+                  label="Background"
+                  type="text"
+                />
+
+                <v-spacer />
+                <v-row class="justify-center">
+                  <v-btn
+                    :disabled="!valid"
+                    type="submit"
+                    color="primary"
+                  >
+                    Create
+                  </v-btn>
+                </v-row>
+              </v-form>
+              <v-progress-circular
+                v-if="creating"
+                indeterminate
+                color="primary"
+              />
             </v-card-text>
           </div>
         </v-expand-transition>
@@ -54,12 +77,34 @@ import { mapGetters } from 'vuex';
 export default {
   name: 'Boards',
   data: () => ({
-    show: false
+    creating: false,
+    show: true,
+    valid: false,
+    board: {
+      name: '',
+      background: ''
+    },
+    rules: {
+      required: [value => !!value.trim().length || "Is required"]
+    }
   }),
   computed: {
     ...mapGetters('boards', { findBoards: 'find' } ),
     boards () {
       return this.findBoards().data
+    }
+  },
+  methods: {
+    async createBoard () {
+      if (this.valid) {
+        try {
+          const { Boards } = this.$FeathersVuex.api;
+          const newBoard = await new Boards(this.board).save();
+          console.log(newBoard)
+        } catch (error) {
+          console.error(error);
+        }
+      }
     }
   }
 };
